388fdc30a51705596fc0d6b76757a3f1ed47949d
ZaxYonatan <yonatanz@wix.com>
2023-12-05 11:32:40 +0200

    updated upload-tar-script.sh

--- aws-s3-scripts/upload-tar-script.sh
+++ aws-s3-scripts/upload-tar-script.sh
@@ -9,4 +9,9 @@ TAR_TO_UPLOAD=tars-to-upload/$TAR_NAME
 S3_BUCKET_PATH=s3://wix-code-modules
 S3_DESTINATION=$S3_BUCKET_PATH/$TAR_NAME
 
-aws s3 cp $TAR_TO_UPLOAD $S3_DESTINATION
+# aws s3 cp tars-to-upload/cloud-npm-downloader-18.18.0.0.tar.gz s3://wix-code-modules/cloud-npm-downloader-18.18.0.0.tar.gz
+COMMAND="aws s3 cp $TAR_TO_UPLOAD $S3_DESTINATION"
+
+echo "Running the following command:\n$COMMAND"
+# eval $COMMAND # Uncomment before running the script
+echo "Done uploading new tar to $S3_DESTINATION"
