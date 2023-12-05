ad4acff5381d1742c86e7a356b659b8eb664e6de
ZaxYonatan <yonatanz@wix.com>
2023-12-05 16:14:50 +0200

    renamed to LOCAL_TAR_DESTINATION

--- aws-s3-scripts/upload-tar-script.sh
+++ aws-s3-scripts/upload-tar-script.sh
@@ -4,13 +4,13 @@ ARTIFACT_NAME=cloud-npm-downloader-$MAJOR_NODE_VERSION
 TAR_NAME=$ARTIFACT_NAME.$PACK_VERSION.tar.gz
 
 
-TAR_TO_UPLOAD=tars-to-upload/$TAR_NAME
+LOCAL_TAR_DESTINATION=tars-to-upload/$TAR_NAME
 
 S3_BUCKET_PATH=s3://wix-code-modules
 S3_DESTINATION=$S3_BUCKET_PATH/$TAR_NAME
 
 # aws s3 cp tars-to-upload/cloud-npm-downloader-18.18.0.0.tar.gz s3://wix-code-modules/cloud-npm-downloader-18.18.0.0.tar.gz
-COMMAND="aws s3 cp $TAR_TO_UPLOAD $S3_DESTINATION"
+COMMAND="aws s3 cp $LOCAL_TAR_DESTINATION $S3_DESTINATION"
 
 echo "Running the following command:\n$COMMAND"
 # eval $COMMAND # Uncomment before running the script
