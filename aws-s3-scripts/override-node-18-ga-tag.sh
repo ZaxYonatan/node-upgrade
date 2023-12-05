aaa0208da86ecb94faa00364528effedf8755d29
ZaxYonatan <yonatanz@wix.com>
2023-12-05 11:45:14 +0200

    updated command comment

--- aws-s3-scripts/override-node-18-ga-tag.sh
+++ aws-s3-scripts/override-node-18-ga-tag.sh
@@ -5,7 +5,7 @@ ARTIFACT_NAME=cloud-npm-downloader-$NODE_VERSION
 NODE_18_GA_TAG_FILE_NAME=$ARTIFACT_NAME.json
 GA_TAGS_FOLDER_NAME="ga_tags"
 mkdir -p $GA_TAGS_FOLDER_NAME
-GA_TAG_FILE_DESTINATION="./$GA_TAGS_FOLDER_NAME/$NODE_18_GA_TAG_FILE_NAME"
+GA_TAG_FILE_DESTINATION="$GA_TAGS_FOLDER_NAME/$NODE_18_GA_TAG_FILE_NAME"
 
 GA_TAG_CONTENT="
 {\n
@@ -19,7 +19,7 @@ echo $GA_TAG_CONTENT > $GA_TAG_FILE_DESTINATION
 S3_BUCKET_PATH=s3://wix-code-modules
 S3_DESTINATION=$S3_BUCKET_PATH/$GA_TAGS_FOLDER_NAME/$NODE_18_GA_TAG_FILE_NAME
 
-# aws s3 cp ./cloud-npm-downloader-18.json s3://wix-code-modules/ga_tags/cloud-npm-downloader-18.json
+# aws s3 cp ga_tags/cloud-npm-downloader-18.json s3://wix-code-modules/ga_tags/cloud-npm-downloader-18.json
 COMMAND="aws s3 cp $GA_TAG_FILE_DESTINATION $S3_DESTINATION"
 
 echo "Running the following command:\n$COMMAND"
