40ae622a166239a184d7fe05a3de1e2d441c7879
ZaxYonatan <yonatanz@wix.com>
2023-12-09 12:02:56 +0200

    taring-npms: without dockerfile

--- taring-npms/run.sh
+++ taring-npms/run.sh
@@ -1,7 +1,25 @@
+brew install bzip2, pigz
+
+MAJOR_NODE_VERSION=$(cat ./MAJOR_NODE_VERSION)
 PACK_VERSION=$(cat ./PACK_VERSION)
-CONTAINER_NAME=cloud-npm-downloader-container-$PACK_VERSION
+ARTIFACT_NAME=cloud-npm-downloader-$MAJOR_NODE_VERSION
+NPM_TAR_NAME=$ARTIFACT_NAME.$PACK_VERSION.tar.gz
+
+echo "1. creating dynamic_modules.tar"
+tar cf ./dynamic_modules.tar -C ./dynamic_modules .
 
 
-docker build --progress=plain -t cloud-npm-downloader-$PACK_VERSION -f Dockerfile .
-docker run --name $CONTAINER_NAME cloud-npm-downloader-$PACK_VERSION
-docker cp $CONTAINER_NAME:/app/output .
+echo "2. zipping dynamic_modules.tar.gz"
+pigz -5 ./dynamic_modules.tar
+
+
+echo "3. moving dynamic_modules.tar.gz to cloud-npm-downloader"
+cp ./dynamic_modules.tar.gz ./cloud-npm-downloader
+
+cd ./cloud-npm-downloader
+echo "4. zipping inside cloud-npm-downloader folder"
+tar cvf $NPM_TAR_NAME dynamic_modules.tar.gz -I pigz npm-modules.json npm-modules-change-log.json npm-modules-version.txt dependencies.json npmModules.es6 dynamic_modules_pack_version
+
+echo "5. moving cloud-npm-downloader tar.gz output to ./output"
+mkdir output
+cp ./cloud-npm-downloader/$NPM_TAR_NAME ./output/$NPM_TAR_NAME
